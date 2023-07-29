bb2bdd66a55f7f3730333db942c2e10145666638
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-07-29 14:26:28 -0700

    my last commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,9 @@
 def print_combinations():
     for i in range(10):
         for j in range(i+1, 10):
-            print("{:d}{:d}".format(i, j))
+            if i < 8:
+                print("{:d}{:d}".format(i, j), end = ", ")
+            else:
+                print("{:d}{:d}".format(i, j), end = " ")
 
 print_combinations()
