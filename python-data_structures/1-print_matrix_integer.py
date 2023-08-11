67c3bb2887d73e6b91c0a8fd78ea063164e4a3a4
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-08-11 12:25:02 -0700

    Data structures

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
     if isinstance(matrix, list):
-        for drow in matrix:
+        for row in matrix:
            for element in row:
                 print("{:d} ".format(element), end="")
            print("")
