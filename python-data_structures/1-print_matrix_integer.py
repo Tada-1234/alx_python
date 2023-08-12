4399d39e7db9000e428ff97af93404f592cfad8b
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-08-11 22:42:25 -0700

    Python data structure

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if isinstance(matrix, list):
+        count = 0
         for row in matrix:
-           formatted_row = " ".join("{:1}".format(value) for value in row)
-           print(formatted_row)
+           for element in row:
+               if element == matrix[count][-1]:
+                   print("{:d}".format(element), end="")
+               else:
+                   print("{:d}".format(element), end=" ")
+           print("")
+           count = count + 1
     else:
         print("{:}".format(matrix))
