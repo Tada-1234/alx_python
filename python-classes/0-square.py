a04da75650f07eab3dabc7dd87df439597c20db2
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-09-24 12:21:51 -0700

    Python Classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,17 @@
 class Square:
+    """
+    Square Class
+    
+    A Square class that defines a square by its size.
+    
+    Attributes:
+        size (int): The size of the square.
+    """
     def __init__(self, size=0):
+        """
+        The constructor for Square class.
+        
+        Parameters:
+            size (int, optional): The size of the square. Defaults to 0.
+        """
         self.__size = size
