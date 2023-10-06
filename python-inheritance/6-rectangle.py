e0434553712abe0ac4637988e903e5196bcb4fc7
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-10-06 11:32:39 -0700

    Python inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,46 +1,6 @@
 """
 This module creates a class that inherits from another class
 """
-class BaseGeometry:
-    """
-    This class raises exceptions
-    """
-    def area(self):
-        """
-        Function raises an exception
-    
-        Args: 
-            self: our class
-
-        Returns:
-            Exception
-        """
-        raise Exception("area() is not implemented")
-   
-    def integer_validator(self, name, value):
-        """
-        Function raises an exception
-    
-        Args: 
-            self: our class
-            name: the name of data we are passing
-            value: value of the data(numerical)
-
-        Returns:
-            Exception: if the variable is not valid
-        """
-        if type(value) is not int:
-            raise TypeError(name + " must be an integer")
-        if value <= 0:
-            raise ValueError(name + " must be greater than 0")
-        else:
-            return
-        
-    def __dir__(self):
-        attributes = self.__dir__()  # Get the default dir()                                                                                         
-        filtered_attributes = [attr for attr in attributes if attr != '__init_subclass__']
-        return filtered_attributes
-
 class Rectangle(BaseGeometry):
     """
     This class test the variable
