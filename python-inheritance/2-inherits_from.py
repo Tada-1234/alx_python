f3299ed05469166c464d960736713603634fc2f9
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-10-06 10:04:18 -0700

    Python inheritance

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,17 +1,18 @@
 """
 This module returns true if the object is an instance of a class
 """
-def is_same_class(obj, a_class):
-    """
-    Function checks if its an instance of a class
-    
-    Args: 
-        obj: the object we want to check
-        a_class: the class we want to check against
 
-    Returns:
-        bool: True if an instance and false if not
-    """
+"""
+Function checks if its an instance of a class
+
+Args: 
+     obj: the object we want to check
+     a_class: the class we want to check against
+
+Returns:
+     bool: True if an instance and false if not
+"""
+def is_same_class(obj, a_class):
     for cls in type(obj).mro():
         if cls is a_class:
             return True
