d169b0c9860c14abae4b3af92fd3c135588ee8ab
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-10-06 09:54:43 -0700

    Python inheritance

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -13,3 +13,16 @@ class BaseGeometry:
     """
     def area(self):
         raise Exception("area() is not implemented")
+
+    """
+    Overrides dir() 
+
+    Args:
+        self: the class itself
+    Returns:
+        nothing
+    """
+    def __dir__(self):
+        attributes = super().__dir__()  # Get the default dir()
+        filtered_attributes = [attr for attr in attributes if attr != '__init_subclass__']
+        return filtered_attributes
