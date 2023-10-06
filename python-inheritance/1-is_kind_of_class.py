805e1a4505335d2569f0a4aa296da22cc99ca17d
Tada-1234 <tadalaangellagomonda@gmail.com>
2023-10-06 09:12:47 -0700

    Python inheritance

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -3,23 +3,27 @@ This module checks if the object is an instance of,
 or if the object is an instance of a class that inherited, 
 the specified class.
 """
-
 def is_same_class(obj, a_class):
     """
-    The function checks if the object is an instance of the specified class or 
-    an instance of a class that inherited the specified class.
-    
-    Args: 
-        obj (object): The object to be checked.
-        a_class (type): The class (or a tuple of classes) to check against.
-    
+    Checks if the given object is an instance of, or if the object is an instance of a class
+    that inherited from, the specified class.
+
+    Args:
+        obj (object): The object to check.
+        a_class (type): The class to check against.
+
     Returns:
-        bool: Returns True if 'obj' is an instance of 'a_class' or an instance
-              of a class that inherited 'a_class', otherwise False.
+        bool: True if `obj` is an instance of `a_class` or an instance of a class that
+        inherited from `a_class`, otherwise False.
+
+    Note:
+        Uses `type(obj).mro()` to iterate over the Method Resolution Order (MRO) of `obj`,
+        which includes `obj`'s class, base classes, and the base classes of those base classes,
+        etc., checking if any of them are `a_class`.
     """
-    if type(obj) is a_class: # Check if 'obj' is an exact instance of 'a_class'
+    if type(obj) is a_class:
         return True
-    for cls in type(obj).mro(): # Check if 'obj' is an instance of a class that inherited from 'a_class'
+    for cls in type(obj).mro():
         if cls is a_class:
             return True
     return False
